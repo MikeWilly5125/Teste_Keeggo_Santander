00eb36ccf9105971fe4a9b70022610c2c6b22b79
MikeWilly5125 <113602915+MikeWilly5125@users.noreply.github.com>
2024-07-29 01:52:14 -0300

    format

--- cypress/e2e/pages/shoppingOnline_page.js
+++ cypress/e2e/pages/shoppingOnline_page.js
@@ -2,7 +2,6 @@
 const BASEURL = "https://advantageonlineshopping.com/"
 
 class ShoppingOnline {
-
     campoPesquisa = '//div[@id="mobileSearch"]';
     produto = 16;
     tituloProduto = '//div[@id="Description"]/h1';
@@ -10,47 +9,45 @@ class ShoppingOnline {
     navegaTablet = `//a[contains(text(), 'TABLETS')]`;
     carrinho = '(*//*[@id="menuCart"])[1]';
 
-    acessoAplicacao(){
-        cy.visit(BASEURL)
-        cy.url(BASEURL).should('include', 'https://advantageonlineshopping.com').and('include', 'shopping')
-        cy.log('Acesso realizado')
+    acessoAplicacao() {
+        cy.visit(BASEURL);
+        cy.url(BASEURL).should('include', 'https://advantageonlineshopping.com').and('include', 'shopping');
+        cy.log('Acesso realizado');
     }
 
-    realizarBuscaProduto(PRODUTO){
-        cy.xpath(this.campoPesquisa).type(`${PRODUTO}{enter}`)
+    realizarBuscaProduto(PRODUTO) {
+        cy.xpath(this.campoPesquisa).type(`${PRODUTO}{enter}`);
     }
 
-    selecionarProduto(PRODUTO){
+    selecionarProduto(PRODUTO) {
 
-      let produtos = [16,17,18];
+        let produtos = [16, 17, 18];
 
-        for (let i = 0; i <= (produtos.length -1); i++) {
-           
-           cy.xpath(`//img[@id="${produtos[i]}"]`).click();
+        for (let i = 0; i <= (produtos.length - 1); i++) {
 
-                cy.xpath(this.tituloProduto).invoke('text').then(res =>{
-                    
-                    if (res.includes('TABLET')) {   
-                        cy.wrap(res).should('includes', `${PRODUTO}`)
-                        cy.log('produto encontrado!!')
+            cy.xpath(`//img[@id="${produtos[i]}"]`).click();
 
-                        cy.wait(3000);
-                        cy.xpath(this.adicionarCarrinho).click({force: true});
-                        cy.xpath(this.navegaTablet).click();
-                    } 
-                });
-                
+            cy.xpath(this.tituloProduto).invoke('text').then(res => {
+
+                if (res.includes('TABLET')) {
+                    cy.wrap(res).should('includes', `${PRODUTO}`);
+                    cy.log('produto encontrado!!');
+
+                    cy.wait(3000);
+                    cy.xpath(this.adicionarCarrinho).click({ force: true });
+                    cy.xpath(this.navegaTablet).click();
+                }
+            });
         }
 
-        cy.xpath(this.carrinho).click()
+        cy.xpath(this.carrinho).click();
 
     }
 
-    assertProdutosCarrinho(ASSERT){
-
+    assertProdutosCarrinho(ASSERT) {
         for (let i = 1; i <= 3; i++) {
-            cy.xpath(`//*[@id="shoppingCart"]/table/tbody/tr[${i}]/td[2]`).invoke('text').then(res =>{
-            cy.wrap(res).should('includes', ASSERT).and('not.be.empty')
+            cy.xpath(`//*[@id="shoppingCart"]/table/tbody/tr[${i}]/td[2]`).invoke('text').then(res => {
+                cy.wrap(res).should('includes', ASSERT).and('not.be.empty')
             });
 
         };
@@ -58,15 +55,5 @@ class ShoppingOnline {
         cy.screenshot(`pesquisa por produtos: ${ASSERT}`);
 
     }
-
-
-    
-
-   
-
-    
-
-
-
 
 } export default ShoppingOnline
